9276a830b64d53559d706a75c4756c632fd0be26
ruzik74 <azruz555@gmail.com>
2025-04-19 16:16:34 +0600

    keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,15 +1,28 @@
 from flask import Flask
-from threading import Thread
+import os
+from discord.ext import commands
+import discord
 
 app = Flask(__name__)
 
+# Ваш бот
+bot = commands.Bot(command_prefix="!")
+
 @app.route('/')
 def home():
-    return "Бот работает!"
+    return 'Привет, мир!'
 
-def run():
-    app.run(host="0.0.0.0", port=8080)
+@app.route('/status')
+def status():
+    return 'Бот работает!'
 
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
+# Запуск бота
+@bot.event
+async def on_ready():
+    print(f'Бот {bot.user} подключен!')
+
+def run_bot():
+    bot.run(os.getenv('DISCORD_TOKEN'))  # Получение токена из переменных среды
+
+if __name__ == '__main__':
+    app.run(debug=True)
